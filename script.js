e0b399c40a9133b4375016988783c252bbea4c95
Matt <makeshifthidalgo@hotmail.com>
2019-08-15 14:40:36 -0700

    ES6 classes and subclasses

--- script.js
+++ script.js
@@ -457,7 +457,7 @@ console.log(question.get(ans === question.get("correct")));
 
 //Classes
 //ES5
-
+/*
 var Person5 = function(name, yearOfBirth, job) {
   (this.name = name), (this.yearOfBirth = yearOfBirth), (this.job = job);
 };
@@ -487,3 +487,68 @@ class Person6 {
 
 Person6.greeting();
 const john6 = new Person6("John", 1983, "teacher");
+*/
+
+//Sub-classes
+//ES5
+/*
+var Person5 = function(name, yearOfBirth, job) {
+  (this.name = name), (this.yearOfBirth = yearOfBirth), (this.job = job);
+};
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+};
+
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job);
+  (this.olympicGames = olympicGames), (this.medals = medals);
+};
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+  this.medals++;
+  console.log(this.medals);
+};
+
+var johnAth5 = new Athlete5("John", 1990, "swimmer", 3, 10);
+
+johnAth5.calculateAge();
+johnAth5.wonMedal();
+*/
+
+//ES6
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    (this.name = name), (this.yearOfBirth = yearOfBirth), (this.job = job);
+  }
+
+  calculateAge() {
+    const age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+
+  static greeting() {
+    console.log("Hey there");
+  }
+}
+
+Person6.greeting();
+const john6 = new Person6("John", 1983, "teacher");
+
+class Athlete6 extends Person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job);
+    (this.olympicGames = olympicGames), (this.medals = medals);
+  }
+  wonMedal() {
+    this.medals++;
+    console.log(this.medals);
+  }
+}
+
+const johnAth6 = new Athlete6("John", 1990, "swimmer", 9, 2);
+johnAth6.wonMedal();
+johnAth6.calculateAge();
